6c6d734df2c41b5693f282cd903866d5a74d5ea2
Prasanna venkatesh <tprasanna@identiv.com>
2016-11-23 09:48:53 +0530

    Added some more

---
+++ jobs/app-beta-verify-idod.groovy
@@ -0,0 +1,41 @@
+job('app-beta-verify') {
+    description 'Build and test the app.'
+    scm {
+        git('ssh://idondemandhudson@git.dev.identiv.com:29418/idod/chef/instances/beta')
+    }
+    
+    configure { project ->
+        def matrix = project / 'properties' / 'hudson.security.AuthorizationMatrixProperty' {
+            permission('hudson.model.Item.Build:thu')
+            permission('hudson.model.Item.Workspace:thu')
+            permission('hudson.model.Item.Discover:thu')
+            permission('hudson.model.Item.Read:thu')
+            permission('hudson.model.Item.Cancel:thu')
+        }
+        
+        def param = project / 'properties' / 'hudson.model.ParametersDefinitionProperty' {
+            def paramdef = 'parameterDefinitions' {
+                def strDefinitions = 'hudson.model.StringParameterDefinition' {
+                    name ('GERRIT_REFSPEC')
+                    defaultValue ('refs/heads/master')
+                }
+            }
+        }
+    }
+
+    configure { project ->
+        def trigger = project / 'triggers' / 'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.GerritTrigger' {
+
+    }
+    } 
+
+    steps {
+        gradle {
+            useWrapper true
+            tasks 'build'
+        }
+    }    
+
+
+}
+        
