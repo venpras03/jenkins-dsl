f2e96a9d2fe8c7ba396ec1cecb72aa51757d7ceb
Prasanna venkatesh <tprasanna@identiv.com>
2017-02-16 15:05:40 +0530

    Perfecting api-tool-publish

--- jobs/apitoolpublishidod.groovy
+++ jobs/apitoolpublishidod.groovy
@@ -17,6 +17,10 @@ job('api-tool-publish') {
             permission('hudson.model.Item.Read:thu')
             permission('hudson.model.Item.Cancel:thu')
         }
+
+        project / 'properties' / 'hudson.model.labels.LabelExpression.Or' {
+                'hudson.model.labels.LabelAtom'('windows') 
+        }        
           
         project / 'properties' / 'hudson.plugins.disk__usage.DiskUsageProperty' (plugin:'disk-usage@0.22') {
             diskUsageWithoutBuilds ('14204379')
@@ -117,7 +121,7 @@ job('api-tool-publish') {
             blockBuildWhenUpstreamBuilding ('false')
             keepDependencies ('false')
             concurrentBuild ('true')
-            assignedNode('windows')
+            
         }
 
         project / builders << 'hudson.plugins.gradle.Gradle' (plugin:"gradle@1.23") {
@@ -150,6 +154,7 @@ job('api-tool-publish') {
             fromRootBuildScriptDir ('true')
             useWorkspaceAsHome ('false')
         }
+
 
         project / publishers << 'hudson.tasks.ArtifactArchiver' {
             artifacts ('build/exe/*,build/installer/*,build/distributions/*')
