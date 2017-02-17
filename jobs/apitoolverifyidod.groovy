9582143be8924c92427215aded39b75177adef26
Prasanna venkatesh <tprasanna@identiv.com>
2017-02-17 07:12:22 +0530

    Perfecting api-tool-verify

--- jobs/apitoolverifyidod.groovy
+++ jobs/apitoolverifyidod.groovy
@@ -108,6 +108,7 @@ job('api-tool-verify') {
             keepDependencies ('false')
             assignedNode('windows')
             concurrentBuild ('true')
+            name ('api-tool-verify')
         }
 
         project / builders << 'hudson.plugins.msbuild.MsBuildBuilder' (plugin:"msbuild@1.16") {
