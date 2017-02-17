78ae4624b63ebad93a15571d893e4b6775532916
Prasanna venkatesh <tprasanna@identiv.com>
2017-02-17 07:48:43 +0530

    Perfecting app-beta-deploy

--- jobs/appbetadeployidod.groovy
+++ jobs/appbetadeployidod.groovy
@@ -1,8 +1,7 @@
 job('app-beta-deploy') {
     description ''
-    
     configure { project ->
-        
+
         project / 'properties' / 'hudson.model.ParametersDefinitionProperty' {
             'parameterDefinitions' {
                 'hudson.model.StringParameterDefinition' {
@@ -10,9 +9,17 @@ job('app-beta-deploy') {
                     defaultValue ('refs/heads/master')
                 }
             }
-        }        
+        }
 
-        project / 'scm' (class:'hudson.plugins.git.GitSCM', plugin:'git@2.2.12') << {
+        // Throttle property
+        project / 'properties' / 'hudson.plugins.throttleconcurrents.ThrottleJobProperty' (plugin:'throttle-concurrents@1.9.0') {
+            maxConcurrentPerNode ('0')
+            maxConcurrentTotal ('0')
+            throttleEnabled ('false')
+            throttleOption ('project')
+        }            
+
+        project / 'scm' (class:'hudson.plugins.git.GitSCM', plugin:'git@3.0.5') << {
             configVersion ('2')
             'userRemoteConfigs' {
                 'hudson.plugins.git.UserRemoteConfig' {
@@ -20,7 +27,7 @@ job('app-beta-deploy') {
                     url ('ssh://idondemandhudson@git.dev.identiv.com:29418/idod/chef/instances/beta')
                     credentialsId ('b4b11ae3-8b97-4ea4-955e-478d2b93d478')
                 }
-            }      
+            }
             'branches' {
                 'hudson.plugins.git.BranchSpec' {
                     name ('master')
@@ -35,8 +42,7 @@ job('app-beta-deploy') {
                     }
                 }
             'hudson.plugins.git.extensions.impl.CleanCheckout' {}
-            }   
-
+            }
         }
 
         project / triggers << 'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.GerritTrigger' (plugin:"gerrit-trigger@2.22.0") {     
@@ -69,7 +75,7 @@ job('app-beta-deploy') {
             buildNotBuiltMessage''
             buildUnsuccessfulFilepath''
             customUrl''
-            serverName('__ANY__')
+            serverName('git.dev.identiv.com')
             'triggerOnEvents' {
                 'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.events.PluginChangeMergedEvent' {}
             }
@@ -77,23 +83,37 @@ job('app-beta-deploy') {
             dynamicTriggerConfiguration('false')
             triggerConfigURL()
             triggerInformationAction()
-        }        
-
+        }
 
         project << {
             quietPeriod ('5')
-            canRoam ('true')
+            canRoam ('false')
             disabled ('false')
-            blockBuildWhenDownstreamBuilding ('false')
-            blockBuildWhenUpstreamBuilding ('false')
             keepDependencies ('false')
-            assignedNode('slave01')
+            assignedNode('linux')
         }
 
         project / builders << 'hudson.tasks.Shell' {
-            command ('#!/bin/bash -lx # Load RVM into a shell session *as a function* [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" git clean -fd git reset --hard HEAD #rvm install 2.0.0 rvm use 2.0.0 bundle berks ssh -t app-beta.idondemand.com \'sudo yum clean all\' knife solo cook --no-color app-beta.idondemand.com #knife solo cook --no-color app.ficam-test.idondemand.com knife solo cook --no-color sync-beta.idondemand.com')
+            command ('''#!/bin/bash -lx
+
+            # Load RVM into a shell session *as a function*
+            [[ -s &quot;$HOME/.rvm/scripts/rvm&quot; ]] &amp;&amp; source &quot;$HOME/.rvm/scripts/rvm&quot;
+
+            git clean -fd
+            git reset --hard HEAD
+
+            #rvm install 2.0.0
+            rvm use 2.0.0
+            bundle
+            berks
+            ssh -t app-beta.idondemand.com &apos;sudo yum clean all&apos;
+            knife solo cook --no-color app-beta.idondemand.com
+            #knife solo cook --no-color app.ficam-test.idondemand.com
+            knife solo cook --no-color sync-beta.idondemand.com
+            ''')
         }
-        
+
+
         project / publishers << 'hudson.tasks.Mailer' {
             recipients ('mbutcher@identiv.com, tprasanna@identiv.com')
             dontNotifyEveryUnstableBuild('false')
@@ -108,11 +128,10 @@ job('app-beta-deploy') {
                 color ('BLUE')
                 completeBuild ('true')
             }
-        }     
+        }
 
-        project / buildWrappers << 'hudson.plugins.ansicolor.AnsiColorBuildWrapper' (plugin:"ansicolor@0.4.1") {
+        project / buildWrappers << 'hudson.plugins.ansicolor.AnsiColorBuildWrapper' (plugin:"ansicolor@0.4.3") {
             colorMapName ('gnome-terminal')
-        }       
+        }
     }
 }
-        
