ac6a8117638517ea549de6790f2b217ab78b49fa
Prasanna venkatesh <tprasanna@identiv.com>
2017-03-03 11:14:41 +0530

    class modified

--- jobs/lib/GTemplate.groovy
+++ jobs/lib/GTemplate.groovy
@@ -1,14 +1,14 @@
 package lib
-
+import javaposse.*
 
 class GTemplate {
  static def myClosure = { nodaysToKeep, nonumToKeep, noartifactDaysToKeep, noartifactNumToKeep ->
-         logRotator {
+        logRotator{
             daysToKeep(nodaysToKeep)
             numToKeep(nonumToKeep)
             artifactDaysToKeep(noartifactDaysToKeep)
             artifactNumToKeep(noartifactNumToKeep)
-        } 
+        }
     }
 }
 
