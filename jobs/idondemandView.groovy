2193a74c7eaf3897a44eb8f8bec00ba41eb1e2a6
Prasanna venkatesh <tprasanna@identiv.com>
2017-03-29 17:19:17 +0530

    New additions

--- jobs/idondemandView.groovy
+++ jobs/idondemandView.groovy
@@ -140,7 +140,8 @@ job('iwc-verify') {
     configure otherConfigurations (otherConfigs)
     configure gradleConfigurations (gradleConfigs) 
     configure executeShell ('''git clean -fdx && git reset --hard HEAD
-                            echo WIX_ROOT_DIR: $WIX_ROOT_DIR''')
+                            echo WIX_ROOT_DIR: $WIX_ROOT_DIR
+                            ''')
 
     configure {project ->
             project / publishers << 'com.chikli.hudson.plugin.naginator.NaginatorPublisher' (plugin:"naginator@1.9") {
